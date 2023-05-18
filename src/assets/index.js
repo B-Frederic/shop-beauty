dacfc8ebf05d01c7b7217ccfc182450472f9ec9e
B-Frederic <87258430+B-Frederic@users.noreply.github.com>
2023-05-18 17:33:23 +0200

    add: products list and filter

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,5 +4,6 @@ import Hero from "./img/hero.png";
 import Shade from "./img/shade.png";
 import Before from "./img/before.png";
 import After from "./img/after.png";
+import Plane from "./img/plane.png"
 
-export {Logo, Hero, Shade, After, Before};
+export {Logo, Hero, Shade, After, Before, Plane};
