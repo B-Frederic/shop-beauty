ef2080fd50da4d19a2360478ba25df20f323e006
B-Frederic <87258430+B-Frederic@users.noreply.github.com>
2023-05-18 08:52:10 +0200

    init: project shop-beauty

---
+++ src/data/products.js
@@ -0,0 +1,136 @@
+import img1 from "../assets/img1.png";
+import img2 from "../assets/img2.png";
+import img3 from "../assets/img3.png";
+import img4 from "../assets/img4.png";
+import img5 from "../assets/img5.png";
+import img6 from "../assets/img6.png";
+import img7 from "../assets/img7.png";
+import img8 from "../assets/img8.png";
+import img9 from "../assets/img9.png";
+import img10 from "../assets/img10.png";
+import img11 from "../assets/img11.png";
+import img12 from "../assets/img12.png";
+import img13 from "../assets/img13.png";
+import img14 from "../assets/img14.png";
+
+export const SliderProducts = [
+  {
+    name: "Peau",
+    detail: "Super soin",
+    price: "25",
+    img: img1,
+  },
+  {
+    name: "Peau",
+    detail: "Super soin",
+    price: "30",
+    img: img2,
+  },
+  {
+    name: "Nature",
+    detail: "Soin naturel",
+    price: "25",
+    img: img3,
+  },
+];
+
+export const ProductsData = [
+  {
+    name: "Peau",
+    detail: "Super soin",
+    price: "25",
+    img: img1,
+    type: "soin de la peau",
+  },
+  {
+    name: "Peau",
+    detail: "Super soin",
+    price: "30",
+    img: img2,
+    type: "soin de la peau",
+  },
+  {
+    name: "Nature",
+    detail: "Soin naturel",
+    price: "25",
+    img: img3,
+    type: "soin de la peau",
+  },
+  {
+    name: "Fondation",
+    detail: "Super soin",
+    price: "25",
+    img: img4,
+    type: "fondation",
+  },
+  {
+    name: "Conditionneur",
+    detail: "Super soin",
+    price: "30",
+    img: img5,
+    type: "conditionneur",
+  },
+  {
+    name: "Nature",
+    detail: "Soin naturel",
+    price: "25",
+    img: img6,
+    type: "soin de la peau",
+  },
+  {
+    name: "Conditionneur",
+    details: "Super conditionneur",
+    price: "30",
+    img: img7,
+    type: "conditionneur",
+  },
+  {
+    name: "Conditionneur",
+    details: "Super conditionneur",
+    price: "30",
+    img: img8,
+    type: "conditionneur",
+  },
+  {
+    name: "Conditionneur",
+    details: "Super conditionneur",
+    price: "30",
+    img: img9,
+    type: "conditionneur",
+  },
+  {
+    name: "Conditionneur",
+    details: "Super conditionneur",
+    price: "30",
+    img: img10,
+    type: "conditionneur",
+  },
+  {
+    name: "Fondation",
+    details: "Nourris votre peau",
+    price: "12",
+    img: img11,
+    type: "fondation",
+  },
+  {
+    name: "Fondation",
+    details: "Nourris votre peau",
+    price: "12",
+    img: img12,
+    type: "fondation",
+  },
+  {
+    name: "Fondation",
+    details: "Nourris votre peau",
+    price: "12",
+    img: img13,
+    type: "fondation",
+  },
+  {
+    name: "Fondation",
+    details: "Nourris votre peau",
+    price: "12",
+    img: img14,
+    type: "fondation",
+  },
+];
