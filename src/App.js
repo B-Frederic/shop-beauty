eb91d9fdbf7f375d9461120615c4e585974eeaaa
B-Frederic <87258430+B-Frederic@users.noreply.github.com>
2023-05-18 11:46:41 +0200

    add: hero section

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 import React from 'react';
 // Components
 import Header from './components/Layout/Header';
+import HeroSection from './components/HeroSection';
 
 const App = () => {
   return (
     <div className="App">
       <Header />
+      <HeroSection />
     </div>
   );
 };
